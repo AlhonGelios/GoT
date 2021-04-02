ce5f22345eecf78ad7169acc1f8b5a6a39aa338d
Gelios <Geliosal39hon@gmail.com>
2021-04-02 12:45:28 +0300

    1

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+        <div class="loadingio-spinner-pulse-ae1b3qx77uv"><div class="ldio-3w04m0wypkm"><div></div><div></div><div></div></div></div>
+    )
+}
+
+export default Spinner;
